0402f99b6fe40b6cb413e36a54611b027caa7b75
Brendan Fox <brendanjohnfox@gmail.com>
2023-06-11 09:19:46 +0200

    connected viewItinerary to backend rather than mockdata, tidied up ViewTrips

--- client/src/components/ViewItinerary.jsx
+++ client/src/components/ViewItinerary.jsx
@@ -16,98 +16,40 @@ import {
     EditablePreview,
   } from '@chakra-ui/react'
 
-  const itineraryMockData = {
-    "Day 1": [
+  const itineraryMockData = [
       {
-        "Location": "British Museum",
+        "Title": "British Museum",
+        "Description": "Take a walk around the British Museum",
         "Latitude": 51.5194,
         "Longitude": -0.1269,
         "Website": "https://www.britishmuseum.org/",
         "Category": "Museum"
       },
       {
-        "Location": "Covent Garden",
+        "Title": "Covent Museum",
+        "Description": "Take a walk around the Covent Garden",
         "Latitude": 51.5129,
         "Longitude": -0.1227,
         "Website": "https://www.coventgarden.london/",
         "Category": "Shopping & Entertainment"
       },
       {
-        "Location": "Tower of London",
+        "Title": "Tower of London",
+        "Description": "Take a walk around the Tower of London",
         "Latitude": 51.5081,
         "Longitude": -0.0759,
         "Website": "https://www.hrp.org.uk/tower-of-london/",
         "Category": "Historic Site"
       },
       {
-        "Location": "Tower Bridge",
+        "Title": "Tower Bridge",
+        "Description": "Take a walk around Tower Bridge",
         "Latitude": 51.5055,
         "Longitude": -0.0754,
         "Website": "https://www.towerbridge.org.uk/",
         "Category": "Landmark"
       }
-    ],
-    "Day 2": [
-      {
-        "Location": "Buckingham Palace",
-        "Latitude": 51.5014,
-        "Longitude": -0.1419,
-        "Website": "https://www.royal.uk/buckingham-palace",
-        "Category": "Historic Site"
-      },
-      {
-        "Location": "Westminster Abbey",
-        "Latitude": 51.4993,
-        "Longitude": -0.1273,
-        "Website": "https://www.westminster-abbey.org/",
-        "Category": "Church"
-      },
-      {
-        "Location": "Big Ben",
-        "Latitude": 51.5007,
-        "Longitude": -0.1246,
-        "Website": "https://www.parliament.uk/bigben",
-        "Category": "Landmark"
-      },
-      {
-        "Location": "The London Eye",
-        "Latitude": 51.5033,
-        "Longitude": -0.1195,
-        "Website": "https://www.londoneye.com/",
-        "Category": "Observation Wheel"
-      }
-    ],
-    "Day 3": [
-      {
-        "Location": "Natural History Museum",
-        "Latitude": 51.4966,
-        "Longitude": -0.1764,
-        "Website": "https://www.nhm.ac.uk/",
-        "Category": "Museum"
-      },
-      {
-        "Location": "Kensington Palace",
-        "Latitude": 51.5051,
-        "Longitude": -0.1877,
-        "Website": "https://www.hrp.org.uk/kensington-palace/",
-        "Category": "Historic Site"
-      },
-      {
-        "Location": "Hyde Park",
-        "Latitude": 51.5074,
-        "Longitude": -0.1657,
-        "Website": "https://www.royalparks.org.uk/parks/hyde-park",
-        "Category": "Park"
-      },
-      {
-        "Location": "Piccadilly Circus",
-        "Latitude": 51.5101,
-        "Longitude": -0.1340,
-        "Website": "https://www.london.gov.uk/about-us/our-building-and-squares/piccadilly-circus",
-        "Category": "Landmark"
-      }
     ]
-}
 
 // Displays a list of trips (title, departure) so user can then, view, edit, or delete a specific trip
 function ViewItinerary () {
@@ -118,11 +60,19 @@ function ViewItinerary () {
     useEffect(() => {
         const fetchItinerary = async() => {
             // TODO - connect up to db and fetch based on trip id
-            // const res = await fetch(`./api/itineraries`)
-            // const data = await res.json()
-            // console.log(data)
+            const res = await fetch(`../api/itineraries/${id}`)
+            console.log('url is: ', `../api/itineraries/${id}`)
+            console.log('res is: ', res)
+            const data = await res.json()
+            console.log('data is: ', data)
+            console.log('json is: ', data[0]['json_result'])
+            console.log('json is: ', JSON.parse(data[0]['json_result']))
+            console.log('json type is: ', typeof data[0]['json_result'])
+            console.log('json parsetypeis: ', typeof JSON.parse(data[0]['json_result']))
+            const itineraryData = JSON.parse(data[0]['json_result'] )
             // setTrips(data)
-            setItinerary(itineraryMockData)
+            setItinerary(itineraryData)
+            //setItinerary(itineraryMockData)
         }
         fetchItinerary()
     }, [])
@@ -131,25 +81,31 @@ function ViewItinerary () {
         //trips && console.log('trips:', trips) && trips.map(([trip_id, trip_destination, trip_time_of_departure, 
         // trip_duration, trip_activities, trip_budget, trip_additional_information, trip_created_on, trip_updated_on ]) => {
           //console.log('trips:', trips) && 
-        itinerary && itinerary["Day 1"].map((activity) => {
+        itinerary && itinerary.map((activity) => {
               // const location = useLocation()
               // const{ id } = location.state
                 return (
-                    <AccordionItem key={activity["Location"]}>
-                        <Editable defaultValue={activity["Location"]}>
+                  <>
+                    {/* <AccordionItem key={activity["Location"]}> */}
+                        <Editable defaultValue={activity["Title"]}>
                         <EditablePreview />
                         <EditableInput />
                         </Editable>
-                        <AccordionIcon/>
+                        <Editable defaultValue={activity["Description"]}>
+                        <EditablePreview />
+                        <EditableInput />
+                        </Editable>
+                        {/* <AccordionIcon/>
                         <AccordionButton/>
-                        <AccordionPanel>
+                        <AccordionPanel> */}
                         Website: <Text as="i">{activity["Website"]}</Text>
                         <br />Category: <Text as="i">{activity["Category"]}</Text>
                         <br /> <Link to={`/edit-trip/TODO}`}> Edit Activity</Link>  | 
                         <Link to="/delete-trip/TODO"> Delete Activity</Link> |
                         <Link to="/view-itinerary/TODO"> Add more activities</Link>  
-                        </AccordionPanel>
-                    </AccordionItem>
+                        {/* </AccordionPanel>
+                    </AccordionItem> */}
+                    </>
                 )
               })
         }
