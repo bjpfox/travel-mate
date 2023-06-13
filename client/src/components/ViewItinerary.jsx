773a517ff3ae4384a50535c1019fad245946adbf
Brendan Fox <brendanjohnfox@gmail.com>
2023-06-13 12:25:52 +0200

    Created some tests for google map coordinates util function

--- client/src/components/ViewItinerary.jsx
+++ client/src/components/ViewItinerary.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import { Link, useLocation, useParams } from "react-router-dom"
 
 import ItineraryMap from "./ItineraryMap"
+import { getMapCenter } from '../utility/mapUtil.js';
+
 
 import {
     Accordion,
@@ -108,33 +110,6 @@ function ViewItinerary () {
       sendPutItinRequest()
       //return navigate("/view-itine") 
           //return false
-      }
-
-      function getMapCenter(itinerary) {
-        let minLat = itinerary[0]["Latitude"]
-        let maxLat = itinerary[0]["Latitude"]
-        let minLng = itinerary[0]["Longitude"]
-        let maxLng = itinerary[0]["Longitude"]
-        itinerary.forEach((location) => {
-          minLat = location["Latitude"] < minLat ? location["Latitude"] : minLat 
-          maxLat = location["Latitude"] > minLat ? location["Latitude"] : maxLat 
-          minLng = location["Longitude"] < minLng ? location["Longitude"] : minLng 
-          maxLng = location["Longitude"] > maxLng ? location["Longitude"] : maxLng 
-        })
-        const height = Math.abs(minLat-maxLat)
-        const centerLat = minLat + (height/2)
-        const width = Math.abs(minLng-maxLng)
-        const centerLng = minLng + (width/2)
-        const center = {
-          "lat": centerLat,
-          "lng": centerLng
-        }
-        console.log('minlat',minLat)
-        console.log('maxlat',maxLat)
-        console.log('minlng',minLng)
-        console.log('maxlng',maxLng)
-        console.log('center',center)
-        return center
       }
 
     useEffect(() => {
